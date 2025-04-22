bc61ae73f6317fc6ce4d1fbb191921b12d29750f
Irtaza Rizvi <Irtaza Rizvi@DESKTOP-G8L3Q1M>
2025-04-22 16:56:20 +0500

    Add project files.

---
+++ irtaza/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("CALCULATOR");
+        Console.WriteLine("enter first number");
+        float a=float.Parse(Console.ReadLine());
+        Console.WriteLine("enter the given operator, +,-,*,/");
+        char i=char.Parse(Console.ReadLine());
+        Console.WriteLine("enter second number");
+        float b=float.Parse(Console.ReadLine());
+
+        if (i == '+')
+            Console.WriteLine("a+b =" + (a + b));
+        else if (i == '-')
+
+            Console.WriteLine("a-b = " + (a - b));
+        else if (i == '*')
+            Console.WriteLine("axb=" + (a * b));
+        else if (i == '/')
+        {
+            if (b != 0)
+                Console.WriteLine("a/b = " + (a / b));
+            else
+                Console.WriteLine("undefined");
+        }
+        
+
+        
+    }
+
+}
